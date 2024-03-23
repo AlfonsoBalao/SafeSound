fa3a782bb0e4561d4b3b08e4fa6c6ba5d258ac7c
Alfonso Balao <balaoonline@gmail.com>
2024-03-23 20:33:52 +0100

    User permissions

--- app/src/main/java/com/example/safesound/MainActivity.kt
+++ app/src/main/java/com/example/safesound/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.safesound
 
+import android.Manifest
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -15,6 +23,35 @@ import com.google.android.material.tabs.TabLayout
 
 
 class MainActivity : AppCompatActivity() {
+
+    /*Propiedad para los permisos, se define aquí para que se registre en los eventos, siempre antes
+    de la función OnCreate()*/
+
+    private val requestMultiplePermissionsLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        val grantedPermissions = permissions.filterValues { it }
+        val deniedPermissions = permissions.filterValues { !it }
+
+        if (grantedPermissions.isNotEmpty()) {
+            // Al menos un permiso concedido
+            val mensaje = "Permisos concedidos: ${grantedPermissions.keys.joinToString()}"
+            Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
+            musicFiles = getAllAudio(this)
+        }
+
+        if (deniedPermissions.isNotEmpty()) {
+            // Al menos un permiso denegado
+            val mensaje = "Permisos rechazados: ${deniedPermissions.keys.joinToString()}"
+            Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+    /* *********************************************************************** */
+    //Almacenaremos una lista de objetos MusicFiles
+    var musicFiles: ArrayList<MusicFiles> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,8 +61,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        initPaginador();
+
+        //Solicitar permisos de la aplicación
+
+        val permisos = arrayOf(
+            Manifest.permission.INTERNET,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        requestMultiplePermissionsLauncher.launch(permisos)
+        //Fin de la solicitud de permisos de la aplicación
+
+        initPaginador()
     }
+
 
     //Función que inicia la paginación y recoge los fragments de canciones y álbumes
     private fun initPaginador() {
@@ -87,11 +136,74 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
+
+    fun getAllAudio(context: Context): ArrayList<MusicFiles> {
+        val tempAudioList = ArrayList<MusicFiles>()
+        val uri: Uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        val projection = arrayOf(
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.DURATION,
+            MediaStore.Audio.Media.DATA, // para la ruta
+            MediaStore.Audio.Media.ARTIST
+        )
+
+        val cursor: Cursor? = context.contentResolver.query(uri, projection, null, null, null)
+        cursor?.use {
+            while (it.moveToNext()) {
+                val album: String = it.getString(0)
+                val title: String = it.getString(1)
+                val duration: String = it.getString(2)
+                val path: String = it.getString(3)
+                val artist: String = it.getString(4)
+
+                val musicFiles = MusicFiles(path, title, artist, album, duration)
+                // tomar Log.e para verificar
+                Log.e("Path: $path", "Album : $album")
+                tempAudioList.add(musicFiles)
+            }
+        }
+        return tempAudioList
+    }
+
+
 }
 
 
 /*
-*  ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager())
+*  public static ArrayList<MusicFiles> getAllAudio(Context context){
+*
+*  ArrayList<MusicFiles> tempAudioList = new ArrayList<>();
+*  Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+*  String[] projection = {
+*                          MediaStore.Audio.Media.ALBUM,
+*                           MediaStore.Audio.Media.TITLE,
+*                           MediaStore.Audio.Media.DURATION,
+*                           MediaStore.Audio.Media.DATA, //FOR PATH.
+*                           MediaStore.Audio.Media.ARTIST
+*                           };
+*
+*  Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null);
+*   if (cursor != null){
+*           while (cursor.moveToNext())
+*               {
+*                   String album = cursor.getString(0);
+*                   String title = cursor.getString(1);
+*                   String duration = cursor.getString(2);
+*                   String path = cursor.getString(3);
+*                   String artist = cursor.getString(4);
+*
+*                   MusicFiles musicFiles = new MusicFiles(path, title, artist, album, duration);
+*                   //take log.e for check
+*                   Log.e("Path: " + path, "Album : " + album);
+*                   tempAudioList.add(musicFiles;)
+*                   }
+*            cursor.close();
+*           }
+*           return tempAudioList;
+*
 * }
+*       ArrayList<MusicFiles> musicFiles;
 
 * */
---
+++ app/src/main/java/com/example/safesound/MusicFiles.kt
@@ -0,0 +1,9 @@
+package com.example.safesound
+
+class MusicFiles(path: String, title: String, artist: String, album: String, duration: String) {
+    var path: String = ""
+    var title: String = ""
+    var artist: String = ""
+    var album: String = ""
+    var duration: String = ""
+}
